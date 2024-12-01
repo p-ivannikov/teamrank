95ad3c6b2c9c6897999274a8438a17530f004056
Pavel Ivannikov <pavel.a.ivannikov@gmail.com>
2024-12-01 16:02:42 +0400

    fix render

--- apps/teamrank/src/main.tsx
+++ apps/teamrank/src/main.tsx
@@ -5,7 +5,7 @@ import WebApp from '@twa-dev/sdk';
 
 WebApp.ready();
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <App />
   </StrictMode>
