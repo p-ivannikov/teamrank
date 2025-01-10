3aaf4711cd4933d2f21272cde5555b16d29d6fe0
Pavel Ivannikov <pavel.a.ivannikov@gmail.com>
2025-01-10 10:44:00 +0400

    up config

--- apps/teamrank/vite.config.ts
+++ apps/teamrank/vite.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import basicSsl from '@vitejs/plugin-basic-ssl';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
   root: __dirname,
@@ -14,7 +15,7 @@ export default defineConfig({
   //   port: 4300,
   //   host: 'localhost',
   // },
-  plugins: [react(), basicSsl()],
+  plugins: [react(), basicSsl(), nxViteTsPaths()],
   build: {
     outDir: '../../dist/apps/teamrank',
     emptyOutDir: true,
